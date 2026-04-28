e32229409f88462af69ac5752cd2edea23787232
hunaa2006 <hunaahun0@gmail.com>
2026-04-28 22:20:26 +0800

    chore: cleanup git index and ignore node_modules

---
+++ partA/lib/index.ts
@@ -0,0 +1,4 @@
+export type { ICache } from "./api/ICache";
+export { CacheFactory } from "./api/CacheFactory";
+export type { CacheStrategy, CacheFactoryOptions, TTLCacheOptions } from "./api/CacheFactory";
+export { CacheError, CacheErrorCode } from "./api/CacheError";
